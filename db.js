97bd54d4ea09c3001745bc68335e7799ba9ced21
Thomas Peklak <thomas.peklak@gmail.com>
2013-11-13 08:01:29 +0100

    update levelup and leveldown
    byteup is no longer working with the current levelup, the implementation was inlined into this moddule

--- db.js
+++ db.js
@@ -1,19 +1,19 @@
 var async = require('async')
 var bcrypt = require('bcrypt')
-var byteup = require('byteup')
 var levelup = require('levelup')
 var through = require('through')
+var bytewise = require('bytewise')
 
 var PREFIX = "user:"
 
 // Turn an email into a key
 function k(email) {
-  return [PREFIX, email]
+  return bytewise.encode([PREFIX, email]);
 }
 
 // Turn a key into an email
 function dk(k) {
-  return k[1]
+  return bytewise.decode(k)[1]
 }
 
 function genTimestamp(dt) {
@@ -61,10 +61,8 @@ module.exports = function(db) {
     name = db
   }
   if (!db || typeof db === 'string') {
-    // Install the bytewise leveldb plugin
-    byteup()
     db = levelup(name, {
-      keyEncoding: 'bytewise',
+      keyEncoding: 'binary',
       valueEncoding: 'json'
     })
   }
@@ -86,7 +84,7 @@ module.exports = function(db) {
       if (err) return cb(err)
       user.modifiedDate = new Date(user.modifiedTimestamp.unixtime)
       user.createdDate = new Date(user.createdTimestamp.unixtime)
-      user.email = email
+      user.email = email;
 
       return cb(null, user)
     })
@@ -139,7 +137,8 @@ module.exports = function(db) {
         user.modifiedTimestamp = genTimestamp()
         self.batch()
           .del(k(email))
-          .put(k(newEmail), user)
+          // it's strange that we have to specify the valueEnconding here again
+          .put(k(newEmail), user, {valueEncoding: "json"})
           .write(function(err) {
             done()
             cb.call(null, arguments)
@@ -162,7 +161,7 @@ module.exports = function(db) {
             return cb(err)
           }
           userObj.modifiedTimestamp = genTimestamp()
-          userObj.data = user.data
+          userObj.data = user.data;
           self.put(k(email), userObj, function(err) {
             done()
             cb.call(null, arguments)
@@ -180,7 +179,7 @@ module.exports = function(db) {
     var self = this
     writeQ.push(function(done) {
       self.findUser(email, function(err, user) {
-        if (err) { 
+        if (err) {
           done()
           return cb(err)
         }
@@ -199,7 +198,7 @@ module.exports = function(db) {
       var u = data.value
       u.modifiedDate = new Date(u.modifiedTimestamp.unixtime)
       u.createdDate = new Date(u.createdTimestamp.unixtime)
-      u.email = dk(data.key)
+      u.email = dk(data.key);
       this.queue(u)
     }))
   }).bind(db)
